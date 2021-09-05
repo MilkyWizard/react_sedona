f7f9631134ee740febfbde761d856c92cdf473c2
MilyWizard <mr.creeper1226@gmail.com>
2021-09-05 22:34:02 +0300

    111

--- src/Top.js
+++ src/Top.js
@@ -1,15 +1,57 @@
 import React from "react";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 function Top () {
     return (
-        <ul>
-            <li>Главная</li>
-            <li> Фото и видео</li>
-            <li></li>
-            <li>Форма отзыва</li>
-            <li>HTML Academy</li>
-        </ul>
-    )
+      <Router>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Главная</Link>
+                </li>
+                <li>
+                  <Link to="/media">Фото и видео</Link>
+                </li>
+                <li>
+                  <Link to="/">Лого</Link>
+                </li>
+                <li>
+                  <Link to="/form">Форма отзыва</Link>
+                </li>
+                <li>
+                  <Link to="/academy">HTML Academy</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+          <main>
+            <Switch>
+              <Route path="/media">
+                <Media />
+              </Route>
+              <Route path="/form">
+                <Form />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </main>
+        </Router>
+      );
 }
+
+const Home = () => <h2>Главная</h2>;
+
+const Media = () => <h2>Фото и видео</h2>;
+
+const Form = () => <h2>Форма отзыва</h2>;
+
 export default Top;
